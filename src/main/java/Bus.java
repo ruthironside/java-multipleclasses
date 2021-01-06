f84819d8b0475ed4e940ceaf96d7ace86a2ee10a
Ruth Ironside <ruthironside@live.com>
2021-01-06 16:12:36 +0000

    mvp almost achieved

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -2,18 +2,41 @@ import java.util.ArrayList;
 
 public class Bus {
 
+
     private String destination;
     private Integer capacity;
     private ArrayList<Person> passengers;
 
     public Bus(String destination) {
         this.destination = destination;
-        this.capacity = 100;
+        this.capacity = 2;
         this.passengers = new ArrayList<Person>();
     }
 
     public int passengerCount() {
         return this.passengers.size();
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public boolean spaceAvailable() {
+        int totalPassengers = passengerCount();
+        return totalPassengers < this.capacity;
+    }
+
+    public void addPassenger(Person person) {
+        int totalPersons = passengerCount();
+        if (totalPersons < this.capacity) {
+            this.passengers.add(person);
+        }
 
     }
+
+    public Person removePerson() {
+        return this.passengers.remove(0);
+
+    }
+
 }
--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -6,17 +6,52 @@ import static org.junit.Assert.assertEquals;
 public class BusTest {
 
     private Bus bus;
-
+    private Person person;
 
     @Before
     public void before() {
         bus = new Bus("Edinburgh");
-
+        person = new Person();
     }
 
     @Test
     public void busStartsEmpty() {
         assertEquals(0, bus.passengerCount());
     }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(2, bus.getCapacity());
+    }
+
+    @Test
+    public void canTakePassengers() {
+        assertEquals(true, bus.spaceAvailable());
+    }
+
+    @Test
+    public void canAddAPassenger() {
+        bus.addPassenger(person);
+        assertEquals(1, bus.passengerCount());
+    }
+
+    @Test
+    public void busFull() {
+        bus.addPassenger(person);
+        bus.addPassenger(person);
+        assertEquals(false, bus.spaceAvailable());
+    }
+
+    @Test
+    public void removePassenger() {
+
+        Person removedPerson = bus.removePerson(person);
+        assertEquals(removedPerson, person);
+        assertEquals(0, bus.passengerCount());
+    }
+
+
+
+
 
 }
